6bbfbfa48e814d5a2a90f0489ecaeb162c69bae3
MirnaB <baak.mirna@gmail.com>
2013-12-12 11:40:05 +0100

    rearrange 12-12-2013

--- code/galaxy_code/converter_dta_to_csv.py
+++ code/galaxy_code/converter_dta_to_csv.py
@@ -1,13 +1,14 @@
 #Converter of dta to csv
 #Mirna Baak
-#baak.mirna@gmail.com
 #9-10-2013
-#last update: 9-10-2013
+#last update: 13-11-2013
 
 import sys
-file_name = raw_input('Name file: ')
-file_outputname = raw_input('Outputname: ')
-#input_file_name = sys.argv[1]
+import os
+
+file_name = sys.argv[1]
+file_outputname = sys.argv[2]
+file_outputname2 = sys.argv[3]
 
 def convert_dta(file_name,file_outputname):
     
@@ -15,6 +16,7 @@ def convert_dta(file_name,file_outputname):
     a = len(read_file.readlines())
     read_file2 = open(file_name, 'r') # open file second time
     output = open(file_outputname,'w') #output file
+    output2 = open(file_outputname2, 'w')
 
     numberlandmarks = 0
     header = "" 
@@ -25,16 +27,17 @@ def convert_dta(file_name,file_outputname):
         split_tabs = b.split('  ')
         number_columns = len(split_tabs)
         if x == 0:
-            header += b       #name of sample     
+            header += b.replace(' ', '_')     #name of sample     
         if number_columns == 3: #coordinates
-            output.write("%s,%s,%s\n"%(float(split_tabs[0]),float(split_tabs[1]),float(split_tabs[2])))
+            output.write("%f,%f,%f\n"%(float(split_tabs[0]),float(split_tabs[1]),float(split_tabs[2])))
             numberlandmarks += 1 # number of landmarks
 
-    header += ", %s"%(str(numberlandmarks))
-    output.write("%s"%(header)) # writing header to output file
+    output2.write("%s\n"%(header[1:-4])) # writing header to output file
 
     output.close()
-    #outputname.close()
+
 convert_dta(file_name,file_outputname)
+            
+
             
 
--- test.py
+++
@@ -1,8 +0,0 @@
-#13-9-2013
-#Mirna
-
-print 'Dit is een test'
-print 'En werkt het al?'
-print 'ik geloof het wel'
-
-# ik zal het nooit meer doen
