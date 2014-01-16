b6cc8fe8fea69a72534b642ef1d49c84160bfa05
MirnaB <baak.mirna@gmail.com>
2014-01-16 14:55:41 +0100

    rotation version 3

--- code/holes_dents_code/landmark reconstruct/testrotation.py
+++ code/holes_dents_code/landmark reconstruct/testrotation.py
@@ -3,14 +3,14 @@
 import math
 from math import *
 import sys
-#file_name = raw_input("file: ")
-file_name = 'curvation_lower.dta'
+file_name = raw_input("file: ")
+#file_name = 'curvation_lower.dta'
 
 #file_connect = raw_input("file: ")
-file_connect= 'mirror.txt'
-#file_name = 'dart_landmark2.dta'
+#file_connect= 'mirror.txt'
+#file_name = 'dart_landmark.dta'
 #file_connect = raw_input("file: ")
-#file_connect= 'mirror2.txt'
+file_connect= 'mirror.txt'
 
 #maken dat die connect file onthoud. 
 
@@ -18,11 +18,23 @@ file_outputname = 'outputrotate.dta'
 file_outputname2 = 'outputrotate2.dta'
 file_outputname3 = 'outputrotate3.dta'
 file_outputname4 = 'outputrotate4.dta'
+file_outputname5 = 'outputcorrected.dta'
 output = open(file_outputname, 'w')
 output2 = open(file_outputname2, 'w')
 output3 = open(file_outputname3, 'w')
-output4 = open(file_outputname4, 'w') 
-
+output4 = open(file_outputname4, 'w')
+output5 = open(file_outputname5, 'w')
+while True:
+    site = raw_input("left site is good:(Yes or No):  ").upper().strip()
+    site.strip()
+    if site == 'YES' or site == 'Y':
+        site = 0
+        break
+    elif site == 'NO' or site == 'N':
+        site = 1
+        break
+    else:
+        print 'not the right input, try again'
 datapoints = open(file_name)
 datalines = datapoints.readlines()
 listdata = []
@@ -42,7 +54,7 @@ for a  in range(0,len(datalines)):
         sublistje = []
     else:
         output.write("%s\n"%(aline))
-print listdata
+#print listdata
 
 # open mirrorpoints file
 mirrorpoints = open(file_connect)
@@ -87,19 +99,58 @@ for x in range(0,len(listdata)):
     output.write("%s %s %s\n"%(listdata3[x][0], listdata3[x][1], listdata3[x][2]))
 output.close()
 
-# Rotate around z axis ########################################################################
 
+###################################################################################################################################################### 
 
 def Rz_matrix(z_angle): # Rz rotation matrix
     return [[cos(math.radians(z_angle)), -sin(math.radians(z_angle)), 0.0],[sin(math.radians(z_angle)),cos(math.radians(z_angle)),0.0],[0.0, 0.0, 1.0]]
 
+
 def Z_rotation(point2, z_angle): # multiplication rotation matrix and coordinates 
     r_z = Rz_matrix(z_angle)
-    #print 'm', r_2
     rotated_z = []               
     for i in range(3):
         rotated_z.append((sum([r_z[i][j] * point2[j] for j in range(3)])))
     return rotated_z
+
+
+def Rx_matrix(x_angle): #rotation matrix x-axis
+    return [[1, 0, 0],[0,cos(math.radians(x_angle)),-sin(math.radians(x_angle))],[0,sin(math.radians(x_angle)),cos(math.radians(x_angle))]]
+
+def X_rotation(point3, x_angle): #multiplication rotation matrix and coordinates 
+    r_x = Rx_matrix(x_angle)
+    rotated_x = []  
+    for i in range(3):
+        rotated_x.append((sum([r_x[i][j] * point3[j] for j in range(3)])))
+    return rotated_x
+
+
+def Ry_matrix(y_angle): # Ry rotation matrix
+    return [[cos(math.radians(y_angle)), 0.0, sin(math.radians(y_angle))],[0.0, 1.0, 0.0],[-sin(math.radians(y_angle)),0.0, cos(math.radians(y_angle))]]
+
+def Y_rotation(point4, y_angle): #multiplication rotation matrix and coordinates 
+    r_y = Ry_matrix(y_angle)
+    rotated_y = []
+    for i in range(3):
+        rotated_y.append((sum([r_y[i][j] * point4[j] for j in range(3)])))
+    return rotated_y
+
+
+#########################################################################################################################################################
+
+
+# als punt 13 onder punt 15 ligt, moet die 180 graden gedraaid worden over de z as maak eerst temp
+listdatatemp = []
+if listdata3[sym1][1] < listdata3[sym3][1]:
+    print 'iets'
+    angle = 180
+    for coordinates in range(0,len(listdata3)):
+        listdatatemp.append(Z_rotation(listdata3[coordinates], angle))
+    print 'temp', listdatatemp
+    listdata3 = listdatatemp
+print listdata3
+
+# Rotate around z axis ########################################################################
 
 # calculate angle rotation
 len_z_a = listdata3[int(sym1)][0] - listdata3[int(sym3)][0]
@@ -117,34 +168,11 @@ for coordinates in range(0, len(listdata3)):
 print 'list4', listdata4
 
 for x in range(0,len(listdata)):
-    output2.write("%s %s %s\n"%(listdata4[x][0], listdata4[x][1], listdata4[x][2]))
+    output2.write("%.7f %.7f %.7f\n"%(listdata4[x][0], listdata4[x][1], listdata4[x][2]))
 
 output2.close()
-###########################################################################################################################
-# around x-axis #######################################################
 
-def Rx_matrix(x_angle): #rotation matrix x-axis
-    #print cos(math.radians(x_angle))
-    return [[1, 0, 0],[0,cos(math.radians(x_angle)),-sin(math.radians(x_angle))],[0,sin(math.radians(x_angle)),cos(math.radians(x_angle))]]
-
-def X_rotation(point3, x_angle): #multiplication rotation matrix and coordinates 
-    r_x = Rx_matrix(x_angle)
-    #print r_x
-    rotated_x = []
-    for points in range(0,2):#len(listdata3)
-        #print points
-        for i in range(0,3):
-            ax = r_x[i][0] * listdata4[points][0]
-            by = r_x[i][1] * listdata4[points][1]
-            cz = r_x[i][2] * listdata4[points][2]
-            totalx = ax + by + cz
-            #print 'total', totalx
-            #print '\n'
-            
-    for i in range(3):
-        rotated_x.append((sum([r_x[i][j] * point3[j] for j in range(3)])))
-        #round weggehaald
-    return rotated_x
+# around x-axis ##############################################################################
 
 #calculate angle point 13z / point 13y int(sym1)][0]
 len_x_a = listdata4[int(sym1)][2] - listdata4[int(sym3)][0]
@@ -161,21 +189,10 @@ for d in range(0, len(listdata3)):
 print 'listdata5', listdata5
 # write new coordinates
 for x in range(0,len(listdata)):
-    output3.write("%s %s %s\n"%(listdata5[x][0], listdata5[x][1], listdata5[x][2]))
+    output3.write("%.7f %.7f %.7f\n"%(listdata5[x][0], listdata5[x][1], listdata5[x][2]))
 
 
 # Rotate around y axis#############################################################
-def Ry_matrix(y_angle): # Ry rotation matrix
-    return [[cos(math.radians(y_angle)), 0.0, sin(math.radians(y_angle))],[0.0, 1.0, 0.0],[-sin(math.radians(y_angle)),0.0, cos(math.radians(y_angle))]]
-
-def Y_rotation(point4, y_angle): #multiplication rotation matrix and coordinates 
-    r_y = Ry_matrix(y_angle)
-    print r_y
-    rotated_y = []
-    for i in range(3):
-        rotated_y.append((sum([r_y[i][j] * point4[j] for j in range(3)])))
-        #round weggehaald
-    return rotated_y
 
 #calculate angle 4 x coordinate point 3 / z coordinate point 3.
 len_y_a = (listdata5[int(sym2)][0] - listdata5[int(sym3)][0])
@@ -186,51 +203,69 @@ listdata6 = []
 
 # new coordinates in list
 for d in range(0, len(listdata5)):
-    print 's', Y_rotation(listdata5[d], y_angle)
+    #print 's', Y_rotation(listdata5[d], y_angle)
     listdata6.append(Y_rotation(listdata5[d], y_angle))
 
 print 'listdata6', listdata6
+
+listdatatemp = []
+if listdata6[sym1][2] < listdata6[sym3][1]:
+    print 'iets2'
+    angle = 180
+    for coordinates in range(0,len(listdata6)):
+        listdatatemp.append(Y_rotation(listdata6[coordinates], angle))
+    print 'temp', listdatatemp
+    listdata6 = listdatatemp
+print 'listnew', listdata6
+
 # write new coordinates
 for x in range(0,len(listdata5)):
-    output4.write("%s %s %s\n"%(listdata6[x][0], listdata6[x][1], listdata6[x][2]))
+    output4.write("%.7f %.7f %.7f\n"%(listdata6[x][0], listdata6[x][1], listdata6[x][2]))
+
+
     
-'''
-marge = 300 ####### hoe zouden we hier een getal voor kunnen nemen?
+####################################################################################################################################################
+
+#marge = 300 ####### hoe zouden we hier een getal voor kunnen nemen?
 # change the wrong points
 for x in range(0,len(listje)):
+    
+    
     left = listje[x][0]
     right = listje[x][1]
-    co_left = listdata[int(left)-1]
-    co_right = listdata[int(right)-1]
+    print listdata6[int(left)-1][0]
+    marge = abs(listdata6[int(left)-1][0]/2)
+    #marge = 300
+    print 'marge', marge
+    co_left = listdata6[int(left)-1]
+    print co_left
+    co_right = listdata6[int(right)-1]
+    print co_right
     distance_x = float(co_left[0]) + float(co_right[0])
-    #print 'distance', distance_x
+    print 'distance', distance_x
 
     if distance_x > marge:
         if site == 0:
-            listdata[int(right)-1][0] = float(co_left[0]) * -1
-            listdata[int(right)-1][1] = float(co_left[1])
-            listdata[int(right)-1][2] = float(co_left[2])
+            listdata6[int(right)-1][0] = float(co_left[0]) * -1
+            listdata6[int(right)-1][1] = float(co_left[1])
+            listdata6[int(right)-1][2] = float(co_left[2])
         if site == 1:
-            listdata[int(left)-1][0] = float(co_right[0]) * -1
-            listdata[int(left)-1][1] = float(co_right[1])
-            listdata[int(left)-1][2] = float(co_right[2])
+            listdata6[int(left)-1][0] = float(co_right[0]) * -1
+            listdata6[int(left)-1][1] = float(co_right[1])
+            listdata6[int(left)-1][2] = float(co_right[2])
             
-for x in range(0,len(listdata)):
-    output.write("%s %s %s\n"%(listdata7[x][0], listdata7[x][1], listdata7[x][2]))
+for x in range(0,len(listdata6)):
+    output5.write("%.7f %.7f %.7f\n"%(listdata6[x][0], listdata6[x][1], listdata6[x][2]))
 
 #print listdata
 #print len(listdata)
-output.close()
-output2.close()
-output3.close()
-output4.close()
+
 datapoints.close()
 mirrorpoints.close()
 
 
-
-'''
 output.close()
 output2.close()
 output3.close()
 output4.close()
+output5.close()
